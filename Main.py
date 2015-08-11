211201f262a801871a2d3b4e35f5dd99c3179580
Danny Peng <ibxccc123@gmail.com>
2015-08-11 09:29:55 -0400

    Horizontal Victories Checked

--- Main.py
+++ Main.py
@@ -1,5 +1,8 @@
 __author__ = 'Danny Peng'
 
+import sys
+import time
+
 #X in Grid Represents Player X
 #O in Grid Represents Player O
 def decideWinner(input):
@@ -14,15 +17,16 @@ def decideWinner(input):
             str = fileIn.read(1) #reads in file one character at a time
             if not str:
                 break
-            elif not str.isspace(): #If the column placement is not an empty whitespace string that has at least one character in it
+            elif not str.isspace() : #If the column placement is not an empty whitespace string that has at least one character in it
                 if enterGrid(str, grid, counter):  #modifies the grid with the piece of the player using the info of
-                    break #breaks if there is a winner
+                    break
                 # the column they placed their piece along with who they are
                 # (counter % 2 == 0 is the first player, otherwise the second player)
                 counter = counter + 1 #increases the move counter by one
 
 def enterGrid(str, grid, counter):  #modifies the grid that is kept within the file-reading loop in decideWinner()
-    print(counter)
+    print("Turn: ", counter)
+    sys.stdout.flush()  #python needs to force print function, otherwise program ends before printing
     if counter % 2 == 0:
         player = 'X'
     else:
@@ -46,17 +50,54 @@ def enterGrid(str, grid, counter):  #modifies the grid that is kept within the f
     for row in range(6):
         if grid[row][column] is '-':
             grid[row][column] = player
-            print(grid)
+            printGrid(grid)
+            sys.stdout.flush()
             piece = victoryCheck(grid, row, column) #checks to see if Player X or O has won yet
             if piece == 'X' or piece == 'O':
                 print("Player " + piece + " has won at turn ",  (counter + 1))
-                return 1
+                return True
             break #otherwise, breaks out after placing down the player's piece in its rightful column/row and returns back to decideWinner()
+
+def printGrid(grid): #prints the grid from bottom-up in a readable fashion
+    for row in range(5, -1, -1):
+        print(grid[row]) #stacks rows on top of each other
 
 def victoryCheck(grid, row, column):
     piece = grid[row][column]
+    movingRow = row #variable for a changing row
+    movingColumn = column #variable for a changing column
+    horizontalCount = 1;
 
-    return piece
+    #checks for horizontal victory when starting row isn't 0
+    if movingColumn != 0:
+        while movingColumn != 0:
+            if grid[row][movingColumn-1] == piece:
+                horizontalCount = horizontalCount + 1
+                movingColumn = movingColumn - 1
+            else:
+                break
+
+    movingColumn = column #if 0 is reached or there are no (more) left matches, move back to the original row number
+    if movingRow != 6:
+        while movingColumn != 6:
+            if grid[row][movingColumn+1] == piece:
+                horizontalCount = horizontalCount +1
+                movingColumn = movingColumn + 1
+            else:
+                break
+
+    movingColumn = column
+    while movingColumn != 6:  #checks for horizontal victory when starting row is 0
+        if grid[row][movingColumn+1] == piece:
+            horizontalCount = horizontalCount +1
+            movingColumn = movingColumn + 1
+        else:
+            break
+
+    if horizontalCount >= 4:
+        return piece
+    else:
+        return None
 
 def main():
     decideWinner('input.txt') #Extracts the winner from an input.txt
